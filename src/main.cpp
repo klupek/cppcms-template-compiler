c24fbfede7e49d4ba93423b132158f0477540cfd
Rafal Klupa <klupek@gmail.com>
2014-09-05 02:39:09 +0200

    Added argument -o output_file_name.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "parser.h"
 #include <sstream>
 #include <iostream>
+#include <fstream>
 
 void usage(const std::string& self) {
 	std::cerr << self << " [--code(default) | --ast | --parse ] [ -s SKIN NAME ] file1.tmpl file2.tmpl ...\n";
@@ -8,12 +9,13 @@ void usage(const std::string& self) {
 }
 
 int main(int argc, char **argv) {
-
+	std::ofstream out_file;
+	std::ostream* out = &std::cout;
 	std::vector<std::string> files;
 	cppcms::templates::generator::context ctx;
 	ctx.variable_prefix = "content."; // TODO: load defaults
 	enum { code, ast, parse } mode = code;
-
+	bool end_of_options = false;
 	for(int i=1;i<argc;++i) {
 		const std::string v(argv[i]);
 		if(v == "--code") {
@@ -29,6 +31,18 @@ int main(int argc, char **argv) {
 				ctx.skin = argv[i+1];
 				++i;
 			}
+		} else if(v == "--" ){
+			end_of_options = true;
+		} else if(v == "-o" && i + 1 != argc) {			
+			out = &out_file;
+			out_file.open(argv[i+1]);
+			if(!out_file) {
+				std::cerr << "ERROR: could not open " << argv[i+1] << " for writing\n";
+				usage(argv[0]);
+			}
+			i++;		
+		} else if(v[0] == '-') {
+			usage(argv[0]);
 		} else {
 			files.emplace_back(argv[i]);
 		}
@@ -41,9 +55,9 @@ int main(int argc, char **argv) {
 		cppcms::templates::template_parser p(files);
 		p.parse();
 		if(mode == ast) {
-			p.tree()->dump(std::cout);
+			p.tree()->dump(*out);
 		} else if(mode == code) {
-			p.write(ctx, std::cout);
+			p.write(ctx, *out);
 		} else {
 			std::ostringstream oss;
 			p.write(ctx, oss);
